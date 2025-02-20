09c799d94c6ae5d2a344dc07e5e457f0b713ab26
Adauto Marques <adautomarques937@gmail.com>
2025-02-20 09:38:45 -0300

    Incrementando os testes para consumir o Redis

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { obterCodigo2FA } from "../support/db";
 import { LoginPage } from "../pages/loginPage";
 import { DashPage } from "../pages/DashPage";
+import { cleanJobs, getJob } from "../support/redis";
 
 test("Não deve logar quando o codigo de autenticacao e invalido", async ({
   page,
@@ -29,15 +30,22 @@ test("Deve acessar a conta do usuario", async ({ page }) => {
     cpf: "00000014141",
     senha: "147258",
   };
+
+  await cleanJobs();
+
   await loginPage.acessaPagina();
   await loginPage.preencheCpf(usuario.cpf);
   await loginPage.preencheSenha(usuario.senha);
 
-  await page.waitForTimeout(3000);
-  const codigo = await obterCodigo2FA();
+  await page
+    .getByRole("heading", { name: "Verificação em duas etapas" })
+    .waitFor({ timeout: 3000 });
+
+  const codigo = await getJob();
+
+  // const codigo = await obterCodigo2FA(usuario.cpf);
 
   await loginPage.informe2fa(codigo);
-  await page.waitForTimeout(2000);
 
-  expect(await dashPage.obterSaldo()).toHaveText("R$ 5.000,00");
+  await expect(await dashPage.obterSaldo()).toHaveText("R$ 5.000,00");
 });
